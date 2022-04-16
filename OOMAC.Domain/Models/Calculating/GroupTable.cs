65df9dad43b1b736c1b328c2b6fc2971ac26bb53
TSev26 <tsevcu@gmail.com>
2022-04-16 21:36:18 +0200

    GroupTable

---
+++ OOMAC.Domain/Models/Calculating/GroupTable.cs
@@ -0,0 +1,12 @@
+﻿
+namespace OOMAC.Domain.Models.Calculating
+{
+    public class GroupTable
+    {
+        public string ConName { get; set; }
+
+        public int MatchedPlayed { get; set; }
+
+        public int PointsObtained { get; set; }
+    }
+}
--- OOMAC.WPF/ViewModels/Tournament/TournamentSelectedViewModel.cs
+++ OOMAC.WPF/ViewModels/Tournament/TournamentSelectedViewModel.cs
@@ -1,12 +1,10 @@
 ﻿using OOMAC.Domain.Models;
+using OOMAC.Domain.Models.Calculating;
 using OOMAC.EF.Services;
 using OOMAC.WPF.Commands;
 using OOMAC.WPF.Stores;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace OOMAC.WPF.ViewModels
@@ -42,6 +40,25 @@ namespace OOMAC.WPF.ViewModels
 
         public Tournament SelectedTournament => _tournamentStore.SelectedTournament;
 
+        //public List<GroupTable> groupTables => Matches.Select(x => x.ContestantA.ShortName, x => x.)
+
+        
+
+        /*
+        public void superFunction()
+        {
+
+            var groupTablesA = Matches.Select(x => new GroupTable { ConName = x.ContestantA.ShortName, MatchedPlayed = 1, PointsObtained = x.ScoreContestantA }).ToList();//.Concat(Matches.Select(x => new { x.ContestantB.ShortName, V = 1, x.ScoreContestantB }));
+            var groupTablesB = Matches.Select(x => new GroupTable { ConName = x.ContestantB.ShortName, MatchedPlayed = 1, PointsObtained = x.ScoreContestantB }).ToList();
+
+            var groupTable = groupTablesA.Concat(groupTablesB).ToList();
+
+            var sumsInGroupTable = groupTable.GroupBy(s => s.ConName).Select(x => new GroupTable { ConName = x.First().ConName, MatchedPlayed = x.Sum(x => x.MatchedPlayed), PointsObtained = x.Sum(x => x.PointsObtained) }).ToList();
+
+            var resultInOneRow = Matches.Select(x => new GroupTable { ConName = x.ContestantA.ShortName, MatchedPlayed = 1, PointsObtained = x.ScoreContestantA }).Concat(Matches.Select(x => new GroupTable { ConName = x.ContestantB.ShortName, MatchedPlayed = 1, PointsObtained = x.ScoreContestantB })).GroupBy(s => s.ConName).Select(x => new GroupTable { ConName = x.First().ConName, MatchedPlayed = x.Sum(x => x.MatchedPlayed), PointsObtained = x.Sum(x => x.PointsObtained) }).ToList();
+        }
+        */
+
 
         public List<Bracket> Bracket => SelectedTournament?.Brackets.ToList();
 
@@ -59,6 +76,8 @@ namespace OOMAC.WPF.ViewModels
                 OnPropertyChanged(nameof(SelectedBracket));
                 OnPropertyChanged(nameof(IsBracketSelected));
                 OnPropertyChanged(nameof(Matches));
+                OnPropertyChanged(nameof(GroupTablesSum));
+                OnPropertyChanged(nameof(ShowGroupTablesSum));
                 SelectedMatch = null;
             }
         }
@@ -66,6 +85,10 @@ namespace OOMAC.WPF.ViewModels
         public bool IsBracketSelected => SelectedBracket != null;
 
         public List<Match> Matches => SelectedBracket?.Matches.ToList();
+
+        public List<GroupTable> GroupTablesSum => Matches.Where(x => x.Bracket.Round == 0).Select(x => new GroupTable { ConName = x.ContestantA.ShortName, MatchedPlayed = x.HasFinished ? 1 : 0, PointsObtained = x.ScoreContestantA }).Concat(Matches.Where(x => x.Bracket.Round == 0).Select(x => new GroupTable { ConName = x.ContestantB.ShortName, MatchedPlayed = x.HasFinished ? 1 : 0, PointsObtained = x.ScoreContestantB })).GroupBy(s => s.ConName).Select(x => new GroupTable { ConName = x.First().ConName, MatchedPlayed = x.Sum(x => x.MatchedPlayed), PointsObtained = x.Sum(x => x.PointsObtained) }).OrderByDescending(x => x.PointsObtained).ToList();
+
+        public bool ShowGroupTablesSum => SelectedBracket?.Round == 0;
 
         private Match _selectedMatch;
         public Match SelectedMatch
@@ -82,6 +105,7 @@ namespace OOMAC.WPF.ViewModels
                 OnPropertyChanged(nameof(IsMatchZeroZero));
                 OnPropertyChanged(nameof(HasMatchFinished));
                 OnPropertyChanged(nameof(HasMatchNotFinished));
+                OnPropertyChanged(nameof(IsMatchOneOne));
             }
         }
 
@@ -93,5 +117,7 @@ namespace OOMAC.WPF.ViewModels
 
         public bool HasMatchNotFinished => !HasMatchFinished;
 
+
+        public bool IsMatchOneOne => HasMatchFinished && SelectedMatch?.ScoreContestantA == 1 && SelectedMatch?.ScoreContestantB == 1 && SelectedMatch?.Bracket.Round == 0;
     }
 }
